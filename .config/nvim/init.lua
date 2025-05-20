17ba2f4ffe5fd0517a0231739839460ba24b4ab6
Stanislaw Smetanin <smetanin@hey.com>
2025-05-20 18:16:11 +0300

    Add config for nvim

---
+++ .config/nvim/init.lua
@@ -0,0 +1,14 @@
+-----------------------------------------------------
+--  Общие настройки
+-----------------------------------------------------
+vim.opt.number          = true   -- номера строк
+vim.opt.relativenumber  = false   -- относительные номера
+vim.opt.shiftwidth      = 4
+vim.opt.expandtab       = true
+vim.opt.smartindent     = true
+vim.opt.clipboard       = "unnamedplus"
+vim.opt.ignorecase      = true
+vim.opt.smartcase       = true
+vim.opt.signcolumn      = "yes"
+vim.opt.updatetime      = 300    -- чувствительнее к событиям
+vim.opt.termguicolors   = true   -- цветной вывод в GUI/SSH
